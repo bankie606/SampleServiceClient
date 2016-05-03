596c7b5cde87df5677fd1bc330d4c4a71e628de9
bankie606 <frcrescioni@gmail.com>
2016-05-03 11:14:47 -0700

    Added files via upload

---
+++ Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>AUTHOR AUTHORITY</title>
+    <link rel=stylesheet href="css/StyleSheet.css"/>
+</head>
+<body class="mainbody">
+    <form id="form1" runat="server">
+    <div class="animateme">
+        <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged"></asp:GridView>
+        <asp:Button ID="Button1" runat="server" Text="Press Me" OnClick="Button1_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    BookReviewServiceReference1.SampleBookReviewServiceClient bsc = new BookReviewServiceReference1.SampleBookReviewServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+            FillAuthorDropDown();
+    }
+
+    protected void FillAuthorDropDown()
+    {
+        string[] authors = bsc.GetAuthors();
+        DropDownList1.DataSource = authors;
+        DropDownList1.DataBind();
+    }
+
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string author = DropDownList1.SelectedItem.Text;
+        BookReviewServiceReference1.BookLite[] books = bsc.GetBooks(author);
+        GridView1.DataSource = books;
+        GridView1.DataBind();
+    }
+}
